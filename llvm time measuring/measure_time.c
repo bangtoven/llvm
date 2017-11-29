908a22975df187eb08113c2724792a006a4b008e
Jungho Bang <me@bangtoven.com>
2017-11-28 20:03:37 -0500

    using nano seconds instead of micro seconds

--- llvm time measuring/measure_time.c
+++ llvm time measuring/measure_time.c
@@ -1,4 +1,5 @@
 #include <time.h>
+#include <sys/time.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -12,18 +13,27 @@ typedef struct LoopExecTime {
 } LoopExecTime;
 LoopExecTime data[ARRAY_SIZE];
 
+double getCurrentTime() {
+//    struct timeval tv;
+//    gettimeofday(&tv, NULL);
+//    return tv.tv_sec + tv.tv_usec/1000000.0;
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_sec + ts.tv_nsec/1000000000.0;
+}
+
 void prepareMeasuring(unsigned long hash) {
     programID = hash;
 }
 
 void recordEntry(unsigned long index) {
     if (data[index].entryTime > 0) return; // already recorded entry time
-    data[index].entryTime = clock();
+    data[index].entryTime = getCurrentTime();
 }
 
 void recordExit(unsigned long index) {
     if (data[index].exitTime > 0) return; // already recorded exit time
-    data[index].exitTime = clock();
+    data[index].exitTime = getCurrentTime();
 }
 
 void printFinally() {
