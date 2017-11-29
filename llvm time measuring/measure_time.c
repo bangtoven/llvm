260a2b60c6c2d1caf8f4eb97e2604f561c1fa9e3
Jungho Bang <me@bangtoven.com>
2017-11-28 19:47:38 -0500

    direct accessing without iteration to find data.

--- llvm time measuring/measure_time.c
+++ llvm time measuring/measure_time.c
@@ -1,52 +1,41 @@
 #include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-struct LoopExecTime {
-    clock_t entryTime;
-    clock_t exitTime;
-};
-struct LoopExecTime *data;
+#define ARRAY_SIZE 1000
+
 unsigned long programID;
 
-void prepareMeasuring(unsigned long hash, unsigned long count) {
+typedef struct LoopExecTime {
+    clock_t entryTime;
+    clock_t exitTime;
+} LoopExecTime;
+LoopExecTime data[ARRAY_SIZE];
+
+void prepareMeasuring(unsigned long hash) {
     programID = hash;
-    
 }
 
-void recordEntry(unsigned long loop_id) {
-    int index = indexOfLoop(loop_id);
-    if (index != -1) // already accessed
-        return;
-    
-    struct LoopExecTime newLoop;
-    newLoop.loopID = loop_id;
-    newLoop.entryTime = clock();
-    newLoop.exitTime = 0;
-    
-    data[count] = newLoop;
-    count++;
+void recordEntry(unsigned long index) {
+    if (data[index].entryTime > 0) return; // already recorded entry time
+    data[index].entryTime = clock();
 }
 
-void recordExit(unsigned long loop_id) {
-    int index = indexOfLoop(loop_id); // this shouldn't be -1
-    if (data[index].exitTime > 0) // already recorded exit time
-        return;
-    
+void recordExit(unsigned long index) {
+    if (data[index].exitTime > 0) return; // already recorded exit time
     data[index].exitTime = clock();
 }
 
-struct LoopExecData {
-    unsigned long loopID;
-    double duration;
-};
-
 void printFinally() {
     FILE *file=fopen("loop_exec_time.bin", "ab");
-    for (int i=0; i<count; i++) {
-        struct LoopExecData led;
-        led.loopID = data[i].loopID;
-        led.duration = ((double)data[i].exitTime - data[i].entryTime)/CLOCKS_PER_SEC;
-        fwrite(&led,sizeof(struct LoopExecData),1,file);
+    for (int i=0; i<ARRAY_SIZE; i++) {
+        if (data[i].entryTime == 0) break; // empty item
+        
+        double id = programID + i; // hash + index
+        fwrite(&id,sizeof(double),1,file);
+        
+        double duration = ((double)data[i].exitTime - data[i].entryTime)/CLOCKS_PER_SEC;
+        fwrite(&duration,sizeof(double),1,file);
     }
     fclose(file);
 }
