da487d138010b9e165afb8d8ab25d4aa73c04717
Jungho Bang <me@bangtoven.com>
2018-01-09 19:14:07 -0500

    before delete

--- llvm time measuring/measure_time.c
+++ llvm time measuring/measure_time.c
@@ -3,49 +3,53 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/types.h>
 
 #define ARRAY_SIZE 1000
 
 typedef struct LoopExecTime {
-    unsigned long entryTime;
-    unsigned long exitTime;
+    int64_t entryTime;
+    int64_t exitTime;
 } LoopExecTime;
 LoopExecTime data[ARRAY_SIZE];
 
-unsigned long copyCount;
+int64_t copyCount;
 
-unsigned long getCurrentTime() {
+int64_t getCurrentTime() {
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
-    return ts.tv_sec*1000000000.0 + ts.tv_nsec;
+    int64_t nsec = ts.tv_nsec;
+    int64_t sec = ts.tv_sec;
+    int64_t giga = 1000000000;
+    return sec*giga + nsec;
 }
 
-void recordEntry(unsigned long index) {
+void recordEntry(int64_t index) {
     if (data[index].entryTime > 0) return; // already recorded entry time
     data[index].entryTime = getCurrentTime();
 }
 
-void recordExit(unsigned long index) {
+void recordExit(int64_t index) {
     if (data[index].exitTime > 0) return; // already recorded exit time
     data[index].exitTime = getCurrentTime();
 }
 
-void setCopyCount(unsigned long count) {
+void setCopyCount(int64_t count) {
 	copyCount = count;
     memset(data, 0, sizeof(LoopExecTime)*ARRAY_SIZE);
 }
 
-void printFinally(unsigned long programID) {
+void printFinally(int64_t programID) {
     FILE *file = fopen("loop_exec_time.txt", "a"); // for binary it should be ab.
     for (int i=0; i<ARRAY_SIZE; i++) {
         if (data[i].entryTime == 0) continue; // empty item
         
-        unsigned long id = programID*100 + i; // hash + index
-        unsigned long duration = data[i].exitTime - data[i].entryTime;
-        // fwrite(&id,sizeof(unsigned long),1,file);
-        // fwrite(&copyCount,sizeof(unsigned long),1,file);
+        int64_t id = programID*100 + i; // hash + index
+        int64_t duration = data[i].exitTime - data[i].entryTime;
+        // fwrite(&id,sizeof(int64_t),1,file);
+        // fwrite(&copyCount,sizeof(int64_t),1,file);
         // fwrite(&duration,sizeof(double),1,file);
-        fprintf(file, "%ld, %ld, %ld\n", id, copyCount, duration);
+        fprintf(file, "%llu, %llu, %llu\n", id, copyCount, duration);
     }
     fclose(file);
 }
--- llvm time measuring/time.sh
+++ llvm time measuring/time.sh
@@ -3,8 +3,8 @@ count=2
     
 clang -emit-llvm -o p.bc -c measure_time.c
 clang -emit-llvm -o c.bc -c c_test.c
-llvm-link p.bc c.bc -S -o=b.bc
-/Users/bangtoven/Hacking/llvm/build/bin/Debug/opt -loop-simplify -loop-rotate -mem2reg -myunroll -my-depth=$depth -my-count=$count b.bc -f -o b1.bc
+llvm-link p.bc c.bc -S -o=jfdctint.bc
+/Users/bangtoven/Hacking/llvm/build/bin/Debug/opt -loop-simplify -loop-rotate -mem2reg -myunroll -my-depth=$depth -my-count=$count jfdctint.bc -f -o b1.bc
 #mv cfg.main.dot cfg-$depth-$count.main.dot
 lli b1.bc
 # ./a.out loop_exec_time.bin 
